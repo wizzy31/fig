545882abd69a66778d1c14ef0731140f8242f0da
wizzy31 <76542649+wizzy31@users.noreply.github.com>
2021-03-01 18:52:55 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+pkg upgrade
+cd
+git clone https://github.com/wizzy31/fig-
+clear
+cd ./fig-
+bash install.sh
+cd
+cd ./fig-
+npm start
+
+echo "[*] obrigado por utilizar o bot \"darkbot\" dark agradece"
